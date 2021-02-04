6663599909c16a1057bb0f087eddb5bb9bb0edd6
jasmeeto <jasoberai@gmail.com>
2021-02-04 05:34:14 -0500

    Add hiding for guesser

--- src/Room.js
+++ src/Room.js
@@ -5,12 +5,29 @@ import { initiateSocket, disconnectSocket,
 
 import InputTimer from './InputTimer';
 
+function useRadioButtons(name, initialValue=null) {
+  const [value, setState] = useState(initialValue);
+
+  const handleChange = e => {
+    setState(e.target.value);
+  };
+
+  const inputProps = {
+    name,
+    type: "radio",
+    onChange: handleChange
+  };
+
+  return [value, inputProps];
+}
+
 const Room = (props) => {
   let location = useLocation();
   let room = location.pathname;
   const [word, setWord] = useState("");
   const [wordHistory] = useState([]);
   const [counter, setCounter] = useState(0);
+  const [guesserValue, guesserProps] = useRadioButtons("playertype", "cluegiver");
 
   useEffect(() => {
     if (room) initiateSocket(room);
@@ -34,7 +51,29 @@ const Room = (props) => {
       <h3> Copy room link : &nbsp; 
           <input class="link-input" type="text" value={window.location.href} />
       </h3>
-      <h1 id="word-main"> {word} </h1>
+      <form>
+        <div >
+          <label style={{margin:"5px"}}>
+            <input
+              value="cluegiver"
+              checked={guesserValue === "cluegiver"}
+              {...guesserProps}
+            />
+            Clue Giver
+          </label>
+          <label style={{margin:"5px"}}>
+            <input
+              value="guesser"
+              checked={guesserValue === "guesser"}
+              {...guesserProps}
+            />
+            Guesser
+          </label>
+        </div>
+      </form>
+      { guesserValue === "cluegiver" &&
+        <h1 id="word-main"> {word} </h1>
+      }
       <h3> Success Counter: {counter} </h3>
       <button onClick={() => {sendCommand(room, "resetCounter");}}>Reset Counter</button>
       <button onClick={() => {sendCommand(room, "success")}}>Success</button>
@@ -45,6 +84,7 @@ const Room = (props) => {
       <h3>Your Word History</h3>
       <ul>
         {wordHistory.map((val, i, array) => {
+          if (i===0) return;
           let revIndex = array.length - 1 - i;
           let revWord = array[revIndex];
           return <li key={revIndex}>{revWord}</li>
